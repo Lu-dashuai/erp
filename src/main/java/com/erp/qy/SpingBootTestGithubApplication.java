6cd26641179cdf5ec2e3cfa533314b98de9f3ae7
aaaqy11 <928427598@qq.com>
2019-04-10 09:20:52 +0800

    添加了员工表，和其他表的修改

--- src/main/java/com/erp/qy/SpingBootTestGithubApplication.java
+++ src/main/java/com/erp/qy/SpingBootTestGithubApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.gongyongqin.demo.dao")
+@MapperScan("com.erp.qy.dao")
 public class SpingBootTestGithubApplication {
 
 	public static void main(String[] args) {
